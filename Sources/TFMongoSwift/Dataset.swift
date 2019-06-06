d814540eea1d45c9e9f32b16d54a6d091cfdbe7b
Patrick Freed <patrick.freed@mongodb.com>
2019-06-06 18:23:53 -0400

    update api

--- Sources/TFMongoSwift/Dataset.swift
+++ Sources/TFMongoSwift/Dataset.swift
@@ -5,99 +5,222 @@ public enum TFMongoSwiftError: Error {
     case dataCorrupt
 }
 
+private enum Query {
+    case find(filter: Document?, opts: FindOptions)
+    case aggregate(pipeline: [Document], opts: AggregateOptions)
+
+    fileprivate var batchSize: Int32 {
+        switch self {
+        case let .find(_, opts):
+            // this value is always set
+            // swiftlint:disable:next force_unwrapping
+            return opts.batchSize!
+        case let .aggregate(_, opts):
+            // this value is always set
+            // swiftlint:disable:next force_unwrapping
+            return opts.batchSize!
+        }
+    }
+
+    fileprivate func execute(on collection: MongoCollection<Document>) throws -> MongoCursor<Document> {
+        switch self {
+        case let .find(filter, opts):
+            if let filter = filter {
+                return try collection.find(filter, options: opts)
+            }
+            return try collection.find(options: opts)
+        case let .aggregate(pipeline, opts):
+            return try collection.aggregate(pipeline, options: opts)
+        }
+    }
+}
+
 /// Dataset backed by a lazily evaluated cursor. Each batch makes a round trip to the server. Each iteration of the
-/// iterator yields a `TensorGroup` converted from the generic type associated with this Dataset.
-public struct MongoDataset<T: TensorGroupConvertible & Codable>: Sequence where T.TensorType: Combinable {
-    public typealias Iterator = MongoDatasetIterator<T>
-    public typealias Element = T.TensorType
+/// iterator yields a `TensorGroup` converted from the first generic type to the second.
+public struct MongoDataset<C: Codable, T: TensorGroup & InitializableFromSequence>: Sequence where T.SequenceType == C {
+    public typealias Iterator = MongoDatasetIterator<C, T>
+    public typealias Element = T
 
     private let client: MongoClient
-    private let db: String
-    private let collection: String
-    private var batchSize: Int32
+    private let collection: MongoCollection<Document>
+    private var query: Query
 
-    public init(uri: String? = nil,
-                db: String,
-                collection: String,
-                mapping type: T.Type,
-                batchSize: Int32 = 32) throws {
+    private var batchSize: Int32 {
+        get {
+            return self.query.batchSize
+        }
+        set(newSize) {
+            switch self.query {
+            case let .find(filter, opts):
+                let newOpts = FindOptions(batchSize: newSize, limit: opts.limit, projection: opts.projection)
+                self.query = .find(filter: filter, opts: newOpts)
+            case let .aggregate(pipeline, _):
+                let newOpts = AggregateOptions(batchSize: newSize)
+                self.query = .aggregate(pipeline: pipeline, opts: newOpts)
+            }
+        }
+    }
+
+    /// Initialize a `Dataset` from the result of the provided MongoDB aggregation pipeline.
+    /// - SeeAlso: https://docs.mongodb.com/manual/aggregation/
+    public init(uri: String? = nil, db: String, collection: String, pipeline: [Document]) throws {
         if let uri = uri {
             self.client = try MongoClient(uri)
         } else {
             self.client = try MongoClient()
         }
-        self.db = db
-        self.collection = collection
-        self.batchSize = batchSize
+        self.collection = client.db(db).collection(collection)
+        self.query = .aggregate(pipeline: pipeline, opts: AggregateOptions(batchSize: 1))
     }
 
-    public func batched(_ batchSize: Int32) throws -> MongoDataset<T> {
-        return try MongoDataset(db: self.db, collection: self.collection, mapping: T.self, batchSize: batchSize)
+    /// Initialize a `Dataset` from the given MongoDB collection, optionally providing a filter, projection, or limit
+    /// on the number of results.
+    public init(uri: String? = nil,
+                db: String,
+                collection: String,
+                filter: Document? = nil,
+                projection: Document? = nil,
+                limit: Int64? = nil) throws {
+        if let uri = uri {
+            self.client = try MongoClient(uri)
+        } else {
+            self.client = try MongoClient()
+        }
+
+        self.collection = self.client.db(db).collection(collection)
+        let opts = FindOptions(batchSize: 1, limit: limit, projection: projection)
+        self.query = .find(filter: filter, opts: opts)
+    }
+
+    private init(copying dataset: MongoDataset<C, T>) {
+        self.client = dataset.client
+        self.collection = dataset.collection
+        self.query = dataset.query
+    }
+
+    /// Returns a copy of this dataset that populates the `TensorGroup`s yielded by iteration with batches of tensors
+    /// from the given dataset.
+    public func batched(_ batchSize: Int32) throws -> MongoDataset<C, T> {
+        var other = MongoDataset(copying: self)
+        other.batchSize = batchSize
+        return other
     }
 
     public func makeIterator() -> Iterator {
-        let coll = client.db(self.db).collection(self.collection, withType: T.self)
-        guard let cursor = try? coll.find(options: FindOptions(batchSize: self.batchSize)) else {
-            fatalError("error creating cursor")
+        guard let cursor = try? self.query.execute(on: self.collection) else {
+            fatalError("couldn't get cursor")
         }
-        return MongoDatasetIterator(wrapping: cursor, batchSize: self.batchSize)
+        return Iterator(wrapping: cursor, batchSize: self.batchSize)
     }
 }
 
 /// Iterator for a `MongoDataset` backed by a lazily evaluated cursor. Each iteration makes constitutes a round trip to
 /// the server. The values are converted to their `TensorGroup` representation and combined into a single group.
-public struct MongoDatasetIterator<T: TensorGroupConvertible & Codable>: IteratorProtocol
-        where T.TensorType: Combinable {
-    private let cursor: MongoCursor<T>
+public struct MongoDatasetIterator<C: Codable, T: TensorGroup & InitializableFromSequence>: IteratorProtocol
+        where T.SequenceType == C {
+    private let cursor: MongoCursor<Document>
     private let batchSize: Int32
 
-    internal init(wrapping cursor: MongoCursor<T>, batchSize: Int32) {
+    internal init(wrapping cursor: MongoCursor<Document>, batchSize: Int32) {
         self.cursor = cursor
         self.batchSize = batchSize
     }
 
-    public mutating func next() -> T.TensorType? {
-        var tensor: T.TensorType?
+    public mutating func next() -> T? {
+        let decoder = BSONDecoder()
+        var elements: [C] = []
         for _ in 0 ..< self.batchSize {
             guard let record = self.cursor.next() else {
+                if let error = self.cursor.error {
+                    if case UserError.logicError(_) = error {
+                        continue
+                    } else {
+                        print("failed iterating cursor: \(error)")
+                    }
+                }
                 break
             }
-            tensor = tensor?.combined(with: record.tensorValue) ?? record.tensorValue
+            guard let element = try? decoder.decode(C.self, from: record) else {
+                print("failed decoding from \(record)")
+                break
+            }
+            elements.append(element)
         }
-        return tensor
+
+        guard !elements.isEmpty else {
+            return nil
+        }
+
+        return T(from: elements)
     }
 }
 
 extension Dataset {
-    public init<T: TensorGroupConvertible & Codable>(from collection: MongoCollection<T>) throws
-            where T.TensorType == Element, Element: Combinable {
-        let cursor = try collection.find()
-        let tensors = cursor.reduce(nil) { (cumul: T.TensorType?, sample: T) in
-            cumul?.combined(with: sample.tensorValue) ?? sample.tensorValue
+    /// Initialize a `Dataset` from the result of a MongoDB query.
+    /// An optional "transform" function can also be passed in that mutates the entire dataset before it is batched
+    /// (e.g. for normalization).
+    public init<T: Codable>(from cursor: MongoCursor<T>, transform: ((inout Element) -> Void)? = nil)
+            where Element: InitializableFromSequence, Element.SequenceType == T {
+        var elements = Element(from: Array(cursor))
+
+        if let f = transform {
+            f(&elements)
         }
 
-        guard let t = tensors else {
-            throw TFMongoSwiftError.dataCorrupt
-        }
-
-        self.init(elements: t)
+        self.init(elements: elements)
     }
 
-    /// Gets a TensorFlow `Dataset` by converting values read from the given namespace to a `TensorGroup` as per
-    /// the generic `TensorGroupConvertible` type.
-    public init<T: TensorGroupConvertible & Codable>(uri: String? = nil,
-                                                     db: String,
-                                                     collection: String,
-                                                     mapping: T.Type) throws
-            where T.TensorType == Element, Element: Combinable {
+    /// Initialize a `Dataset` from the result of the provided MongoDB aggregation pipeline.
+    /// An optional "transform" function can also be passed in that mutates the entire dataset before it is batched
+    /// (e.g. for normalization).
+    /// - SeeAlso: https://docs.mongodb.com/manual/aggregation/
+    public init<C: Codable>(uri: String? = nil,
+                            db: String,
+                            collection: String,
+                            pipeline: [Document],
+                            outputType: C.Type,
+                            transform: ((inout Element) -> Void)? = nil) throws
+            where Element: InitializableFromSequence, Element.SequenceType == C {
         let client: MongoClient
         if let uri = uri {
             client = try MongoClient(uri)
         } else {
             client = try MongoClient()
         }
-        let coll = client.db(db).collection(collection, withType: T.self)
+        let coll = client.db(db).collection(collection)
+        let cursor = try coll.aggregate(pipeline)
 
-        try self.init(from: coll)
+        let decoder = BSONDecoder()
+        var elements = Element(from: try cursor.map { try decoder.decode(C.self, from: $0) })
+
+        if let f = transform {
+            f(&elements)
+        }
+
+        self.init(elements: elements)
+    }
+
+    /// Initialize a `Dataset` from the given MongoDB collection, optionally providing a filter, projection, or limit
+    /// on the number of results.
+    /// An optional "transform" function can also be passed in that mutates the entire dataset before it is batched
+    /// (e.g. for normalization).
+    public init<C: Codable>(uri: String? = nil,
+                            db: String,
+                            collection: String,
+                            filter: Document = [:],
+                            projection: Document? = nil,
+                            limit: Int64? = nil,
+                            collectionType: C.Type,
+                            transform: ((inout Element) -> Void)? = nil) throws
+            where Element: InitializableFromSequence, Element.SequenceType == C {
+        let client: MongoClient
+        if let uri = uri {
+            client = try MongoClient(uri)
+        } else {
+            client = try MongoClient()
+        }
+        let coll = client.db(db).collection(collection, withType: C.self)
+        let cursor = try coll.find(filter, options: FindOptions(limit: limit, projection: projection))
+        self.init(from: cursor, transform: transform)
     }
 }
--- Sources/TFMongoSwift/TensorGroup.swift
+++ Sources/TFMongoSwift/TensorGroup.swift
@@ -2,51 +2,51 @@ import Foundation
 import MongoSwift
 import TensorFlow
 
-/// This file defines protocols that help facilitate mapping between data stored in MongoDB and `TensorGroup`s.
+/// This file defines protocols and structures that help facilitate mapping between data stored in MongoDB and
+/// `TensorGroup`s.
 
-/// Allows a given `TensorGroup` to be combined (e.g. adding rows) with another `TensorGroup` of the same type.
-public protocol Combinable where Self: TensorGroup {
-    /// Returns a new `TensorGroup` of the same type as `self` which is the result of combining `self` with the supplied
-    /// `TensorGroup`.
-    func combined(with other: Self) -> Self
-}
+/// A basic `TensorGroup` that consisting of a `Tensor` of features and a `Tensor` of labels.
+public struct FLB<T: FeatureLabelMapping>: TensorGroup, InitializableFromSequence {
+    public typealias SequenceType = T
 
-/// A type conforming to `TensorGroup` and `Combinable` that contains two tensors: one for features and one for labels.
-/// Can be used as a generic batch type on a Dataset for a wide variety of applications.
-public struct FeatureLabelBatch<FeatureType: TensorFlowScalar, LabelType: TensorFlowScalar>: TensorGroup, Combinable {
-    public let features: Tensor<FeatureType>
-    public let labels: Tensor<LabelType>
+    /// The features for this batch of data.
+    public var features: Tensor<T.FeatureType>
 
-    public func combined(with other: FeatureLabelBatch<FeatureType, LabelType>)
-                    -> FeatureLabelBatch<FeatureType, LabelType> {
-        return FeatureLabelBatch(features: self.features.concatenated(with: other.features),
-                                 labels: self.labels.concatenated(with: other.labels))
+    /// The labels for this batch of data.
+    public var labels: Tensor<T.LabelType>
+
+    public init<S: Sequence>(from elements: S) where S.Element == SequenceType {
+        var featureScalars: [T.FeatureType] = []
+        var labelScalars: [T.LabelType] = []
+
+        for element in elements {
+            featureScalars += element.features
+            labelScalars.append(element.label)
+        }
+
+        var shape = T.featureShape
+        shape[0] = labelScalars.count
+        self.features = Tensor(shape: shape, scalars: featureScalars)
+        self.labels = Tensor(labelScalars)
     }
 }
 
-/// Allows a type to be converted to a given `TensorGroup`.
-public protocol TensorGroupConvertible {
-    associatedtype TensorType: TensorGroup
-
-    var tensorValue: TensorType { get }
-}
-
 /// Allows defining a direct mapping to a `FeatureLabelBatch`.
-public protocol FeatureLabelMapping: TensorGroupConvertible, Codable
-        where Self.TensorType == FeatureLabelBatch<FeatureType, LabelType> {
+public protocol FeatureLabelMapping: Codable {
     associatedtype FeatureType: TensorFlowScalar
     associatedtype LabelType: TensorFlowScalar
 
-    static var NUM_FEATURES: Int { get }
+    static var featureShape: TensorShape { get }
     var features: [FeatureType] { get }
     var label: LabelType { get }
 }
 
-extension FeatureLabelMapping {
-    public var tensorValue: TensorType {
-        let features = self.features
-        let featuresTensor = Tensor<FeatureType>(shape: [1, features.count], scalars: features)
-        let labels = Tensor<LabelType>([self.label])
-        return FeatureLabelBatch(features: featuresTensor, labels: labels)
-    }
+/// Allows initializing a type from a sequence of another type. Required for mapping a collection's data to a
+/// `TensorGroup`.
+public protocol InitializableFromSequence {
+    /// Can be initialized from a sequence of this type.
+    associatedtype SequenceType
+
+    /// Initialize from a sequence of the associated `SequenceType`.
+    init<S: Sequence>(from sequence: S) where S.Element == SequenceType
 }
